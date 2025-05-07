b41bd8615d315e2dc58c5abd2be7095b02101b57
SaganAmery <69127268+SaganAmery@users.noreply.github.com>
2025-05-07 16:47:57 -0700

    Create .py

---
+++ .py
@@ -0,0 +1,20 @@
+# Mood to Emoji Converter
+
+print("How are you feeling today?")
+mood = input("Type a word to describe your mood: ").lower()
+
+emoji_dict = {
+    "happy": "😊",
+    "sad": "😢",
+    "angry": "😠",
+    "tired": "😴",
+    "excited": "🤩",
+    "bored": "😐",
+    "anxious": "😰",
+    "loved": "❤️"
+}
+
+if mood in emoji_dict:
+    print("Here’s your mood in emoji form:", emoji_dict[mood])
+else:
+    print("I don't know that one yet, but I’m learning!")
